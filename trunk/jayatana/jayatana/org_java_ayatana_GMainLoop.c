81f86398c0b2b2aa70ecfbbdb2618101ed63c96f
danjaredg@gmail.com <danjaredg@gmail.com@dce0b01c-7bc6-f727-cd03-96b0e734480f>
2012-04-19 21:59:54 +0000

    Se mejoran la integración con GMainLoop y la gestion de archivo *.desktop, sin embargo se separa en una rama el soporte para Launcher API

--- trunk/jayatana/jayatana/org_java_ayatana_GMainLoop.c
+++ trunk/jayatana/jayatana/org_java_ayatana_GMainLoop.c
@@ -3,17 +3,19 @@
 #include <pthread.h>
 #include <glib.h>
 #include <gio/gio.h>
+#include <X11/Xlib.h>
 
 GMainLoop *loop;
 
 void *g_main_loop_thread( void *ptr ) {
+	g_type_init();
 	loop = g_main_loop_new(NULL, FALSE);
 	g_main_loop_run(loop);
 }
 
 JNIEXPORT void JNICALL Java_org_java_ayatana_GMainLoop_runGMainLoop
   (JNIEnv *env, jclass thatclass) {
-	g_type_init();
+	XInitThreads();
 	g_thread_init(NULL);
 	g_thread_create(g_main_loop_thread, NULL, TRUE, NULL);
 }
