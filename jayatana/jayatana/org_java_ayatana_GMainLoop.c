c340a663ee4b4e04392f563aecb1e3fe1efb26e4
danjaredg <danjaredg@gmail.com>
2012-04-20 00:20:29 +0000

    Se corrige error por inicialización g_type_init(); y se incluye actualización de iconos y menus en tiempo de ejecución

--- jayatana/jayatana/org_java_ayatana_GMainLoop.c
+++ jayatana/jayatana/org_java_ayatana_GMainLoop.c
@@ -8,14 +8,13 @@
 GMainLoop *loop;
 
 void *g_main_loop_thread( void *ptr ) {
-	g_type_init();
 	loop = g_main_loop_new(NULL, FALSE);
 	g_main_loop_run(loop);
 }
 
 JNIEXPORT void JNICALL Java_org_java_ayatana_GMainLoop_runGMainLoop
   (JNIEnv *env, jclass thatclass) {
-	XInitThreads();
+	g_type_init();
 	g_thread_init(NULL);
 	g_thread_create(g_main_loop_thread, NULL, TRUE, NULL);
 }
