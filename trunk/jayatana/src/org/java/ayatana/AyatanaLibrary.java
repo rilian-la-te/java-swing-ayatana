e8542fece945a2a0dbfc8f42803500642bb25080
danjaredg@gmail.com <danjaredg@gmail.com@dce0b01c-7bc6-f727-cd03-96b0e734480f>
2012-04-20 18:40:00 +0000

    Se corrige error fatal para versiones a 32 bits verificando el soporte para esta plataforma

--- trunk/jayatana/src/org/java/ayatana/AyatanaLibrary.java
+++ trunk/jayatana/src/org/java/ayatana/AyatanaLibrary.java
@@ -35,7 +35,7 @@ import java.util.Properties;
  * @author Jared González
  */
 final public class AyatanaLibrary {
-	public static final String VERSION = "0.7.0";
+	public static final String VERSION = "0.8.0";
 	private static boolean loaded = false;
 	private static boolean successful = false;
 	
