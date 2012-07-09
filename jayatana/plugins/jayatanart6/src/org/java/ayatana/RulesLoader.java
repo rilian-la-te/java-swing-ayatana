f08ed04bcf1d4d6ab1e24a687103d5e0cc031057
danjaredg <danjaredg@gmail.com>
2012-07-09 18:27:40 +0000

    Correción de carga de reglas de configuración de actionMenu y startupWMClass

--- jayatana/plugins/jayatanart6/src/org/java/ayatana/RulesLoader.java
+++ jayatana/plugins/jayatanart6/src/org/java/ayatana/RulesLoader.java
@@ -40,76 +40,80 @@ public class RulesLoader {
 	}
 	
 	private static String testRule(File frules, String titleWindow) {
-		String menuActionClass = System.getProperty("jayatana.menuActionClass");
-		String startupWMClass = System.getProperty("jayatana.startupWMClass");
-		if (startupWMClass == null && menuActionClass == null) {
+		String menuActionClass = null;
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader(frules));
 			try {
-				BufferedReader reader = new BufferedReader(
-						new FileReader(frules));
-				try {
-					String line;
-					String input;
-					String param[];
-					while ((line = reader.readLine()) != null) {
-						if (!line.startsWith("#") && line.contains("\t")) {
-							input = line.replaceAll("\t+", "\t");
-							param = input.split("\t");
-							if (param[0].startsWith("T:")) {
-								if (testTitle(param[0].substring(2), titleWindow)) {
-									if (!"*".equals(param[1]))
-										menuActionClass = param[1];
-									if (param.length == 3)
-										DesktopFile.setStartupWMClassToToolKit(param[2]);
-									return menuActionClass;
-								}
-							} else if (param[0].startsWith("P:")) {
-								if (testProperty(param[0].substring(2))) {
-									if (!"*".equals(param[1]))
-										menuActionClass = param[1];
-									if (param.length == 3)
-										DesktopFile.setStartupWMClassToToolKit(param[2]);
-									return menuActionClass;
-								}
+				String line;
+				String input;
+				String param[];
+				while ((line = reader.readLine()) != null) {
+					if (!line.startsWith("#") && line.contains("\t")) {
+						input = line.replaceAll("\t+", "\t");
+						param = input.split("\t");
+						if (param[0].startsWith("T:")) {
+							if (testTitle(param[0].substring(2), titleWindow)) {
+								if (!"*".equals(param[1]))
+									menuActionClass = param[1];
+								else
+									menuActionClass = DEFAULT_MENU_ACTION_CLASS;
+								if (param.length == 3)
+									DesktopFile.setStartupWMClassToToolKit(param[2]);
+								return menuActionClass;
+							}
+						} else if (param[0].startsWith("P:")) {
+							if (testProperty(param[0].substring(2))) {
+								if (!"*".equals(param[1]))
+									menuActionClass = param[1];
+								else
+									menuActionClass = DEFAULT_MENU_ACTION_CLASS;
+								if (param.length == 3)
+									DesktopFile.setStartupWMClassToToolKit(param[2]);
+								return menuActionClass;
 							}
 						}
 					}
-				} finally {
-					reader.close();
 				}
-			} catch (IOException e) {
-				// ignorar
+			} finally {
+				reader.close();
 			}
-		} else {
-			if (startupWMClass != null)
-				DesktopFile.setStartupWMClassToToolKit(startupWMClass);
+		} catch (IOException e) {
+			// ignorar
 		}
 		return menuActionClass;
 	}
 	
 	public static String load(String titleWindow) {
-		if (titleWindow == null)
-			titleWindow = "unknow";
-		String menuActionClass = null;
-		File frulesDir = new File(System.getProperty("user.home"),".jayatana.rules.d");
-		if (frulesDir.exists() && frulesDir.isDirectory() && frulesDir.canRead())
-			for (File fr : frulesDir.listFiles()) {
-				if (fr.isFile() && fr.canRead()) {
-					if ((menuActionClass = testRule(fr, titleWindow)) != null)
-						return menuActionClass;
+		String menuActionClass = System.getProperty("jayatana.menuActionClass");
+		String startupWMClass = System.getProperty("jayatana.startupWMClass");
+		if (startupWMClass == null && menuActionClass == null) {
+			if (titleWindow == null)
+				titleWindow = "unknow";
+			File frulesDir = new File(System.getProperty("user.home"),".jayatana.rules.d");
+			if (frulesDir.exists() && frulesDir.isDirectory() && frulesDir.canRead())
+				for (File fr : frulesDir.listFiles()) {
+					if (fr.isFile() && fr.canRead()) {
+						if ((menuActionClass = testRule(fr, titleWindow)) != null)
+							return menuActionClass;
+					}
 				}
-			}
-		frulesDir = new File("/etc/jayatana.rules.d");
-		if (frulesDir.exists() && frulesDir.isDirectory() && frulesDir.canRead())
-			for (File fr : frulesDir.listFiles()) {
-				if (fr.isFile() && fr.canRead()) {
-					if ((menuActionClass = testRule(fr, titleWindow)) != null)
-						return menuActionClass;
+			frulesDir = new File("/etc/jayatana.rules.d");
+			if (frulesDir.exists() && frulesDir.isDirectory() && frulesDir.canRead())
+				for (File fr : frulesDir.listFiles()) {
+					if (fr.isFile() && fr.canRead()) {
+						if ((menuActionClass = testRule(fr, titleWindow)) != null)
+							return menuActionClass;
+					}
 				}
+			File frules = new File("/etc/jayatana.rules");
+			if (frules.exists() && frules.isFile() && frules.canRead()) {
+				if ((menuActionClass = testRule(frules, titleWindow)) != null)
+					return menuActionClass;
 			}
-		File frules = new File("/etc/jayatana.rules");
-		if (frules.exists() && frules.isFile() && frules.canRead()) {
-			if ((menuActionClass = testRule(frules, titleWindow)) != null)
-				return menuActionClass;
+		} else {
+			if (startupWMClass != null)
+				DesktopFile.setStartupWMClassToToolKit(startupWMClass);
 		}
 		if (menuActionClass == null)
 			menuActionClass = DEFAULT_MENU_ACTION_CLASS;
