3cebf036c3ffca5da181ae157090049ec59f3bff
danjaredg <danjaredg@gmail.com>
2012-09-13 22:41:52 +0000

    Se completa la integración con endorsed y se incluye un instalador sobre el ant del proyecto principal para facilitar la instalación

--- jayatana/plugins/jayatanart7/src/org/java/ayatana/NetbeansPlatformMenuAction.java
+++ jayatana/plugins/jayatanart7/src/org/java/ayatana/NetbeansPlatformMenuAction.java
@@ -1,28 +1,3 @@
-/*
- * Copyright (c) 2012 Jared González
- * 
- * Permission is hereby granted, free of charge, to any
- * person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the
- * Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the
- * Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice
- * shall be included in all copies or substantial portions of
- * the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
- * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 package org.java.ayatana;
 
 import java.awt.Window;
@@ -30,22 +5,16 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import org.openide.awt.DynamicMenuContent;
+import javax.swing.*;
 
-/**
- * Clase para acciones adiconales para completar la integración con los metodos
- * propopios de netbeans al momento de generar menus dinámicos
- * 
- * @author Jared González
- */
 public class NetbeansPlatformMenuAction extends DefaultExtraMenuAction {
 	private boolean initializeLazyMenu = false;
 	private Method methodDoInitialize;
 	private Method methodDynaModel;
 	private Field fieldDynaModel;
+	
+	private boolean initializeDynamicMenu = false;
+	private Method methodSynchMenuPresenters;
 	
 	private void initializeLazyMenu(Class<?> classMenu) throws NoSuchMethodException, NoSuchFieldException {
 		methodDoInitialize = classMenu.getDeclaredMethod("doInitialize", new Class<?>[] {});
@@ -61,6 +30,12 @@ public class NetbeansPlatformMenuAction extends DefaultExtraMenuAction {
 		if (!methodDynaModel.isAccessible())
 			methodDynaModel.setAccessible(true);
 	}
+	
+	private void initializeDynamicMenu(Class<?> classMenu) throws NoSuchMethodException {
+		methodSynchMenuPresenters = classMenu.getDeclaredMethod("synchMenuPresenters", JComponent[].class);
+		methodSynchMenuPresenters.setAccessible(true);
+	}
+	
 	@Override
 	public boolean allowDynamicMenuBar() {
 		return false;
@@ -72,6 +47,17 @@ public class NetbeansPlatformMenuAction extends DefaultExtraMenuAction {
 			return false;
 		else
 			return super.allowMenuAction(window, menubar, menuitem, selected, shortcut);
+	}
+	
+	private boolean instanceOf(Class<?> cls, String clsName) {
+		if (cls.getName().equals(Object.class.getName()))
+			return false;
+		if (cls.getName().equals(clsName))
+			return true;
+		for (Class<?> c : cls.getInterfaces())
+			if (c.getName().equals(clsName))
+				return true;
+		return instanceOf(cls.getSuperclass(), clsName);
 	}
 	
 	@Override
@@ -92,8 +78,17 @@ public class NetbeansPlatformMenuAction extends DefaultExtraMenuAction {
 							.log(Level.WARNING, "Error invoking LazyMenu", e);
 				}
 			}
-			if (menuitem instanceof DynamicMenuContent) {
-				((DynamicMenuContent)menuitem).synchMenuPresenters(null);
+			if (instanceOf(menuitem.getClass(), "org.openide.awt.DynamicMenuContent")) {
+				try {
+					if (!initializeDynamicMenu) {
+						initializeDynamicMenu(menuitem.getClass());
+						initializeDynamicMenu = true;
+					}
+					methodSynchMenuPresenters.invoke(menuitem, new Object[] {null});
+				} catch (Exception e) {
+					Logger.getLogger(NetbeansPlatformMenuAction.class.getName())
+							.log(Level.WARNING, "Error invoking DynamicMenuContent", e);
+				}
 			}
 		}
 	}
